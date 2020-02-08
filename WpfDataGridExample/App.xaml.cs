02dd4244a7faae6886e144e30fcf753ab4a742a2
voidproc <voidproc@gmail.com>
2020-02-08 14:06:23 +0900

    つくった

---
+++ WpfDataGridExample/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using Prism.Ioc;
+using Prism.Regions;
+using System.Windows;
+using WpfDataGridExample.Views;
+
+namespace WpfDataGridExample
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            containerRegistry.RegisterForNavigation<ExampleView>();
+        }
+
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+
+            Container.Resolve<IRegionManager>().RequestNavigate("ContentRegion", "ExampleView");
+        }
+    }
+}
---
+++ WpfDataGridExample/ViewModels/ExampleViewModel.cs
@@ -0,0 +1,30 @@
+﻿using Prism.Mvvm;
+using System;
+using System.Data;
+
+namespace WpfDataGridExample.ViewModels
+{
+    public class ExampleViewModel : BindableBase
+    {
+        private DataTable _data = null;
+
+        public DataView Data { get; private set; } = null;
+
+        public ExampleViewModel()
+        {
+            _data = new DataTable();
+
+            _data.Columns.Add("Name");
+            _data.Columns.Add("Price", typeof(int));
+
+            _data.Rows.Add("商品A", 500);
+            _data.Rows.Add("商品B", 1200);
+            _data.Rows.Add("商品C", DBNull.Value);
+            _data.Rows.Add("商品D", 2000);
+
+            Data = new DataView(_data);
+
+            //Data.RowFilter = "Price > 1000";
+        }
+    }
+}
